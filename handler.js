d9b915e8dc4240203e46bdead45ce069a2e34315
tareq89 <tareqaziz.aust.cse@gmail.com>
2017-03-29 16:56:48 +0600

    added aws sdk, dynalite

--- handler.js
+++ handler.js
@@ -1,15 +1,23 @@
 'use strict';
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB({endpoint: 'http://localhost:4567'})
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
+module.exports.hello = (event, context, callback) => {  
+
+  dynamo.listTables(function(err, data) {
+    // console.log(data.TableNames);
+    const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      table: data
+      // input: event,
     }),
   };
 
   callback(null, response);
+  });
+  
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
