68900849fdd0323c0aab4e9214b8c89342f1e8d5
tareq89 <tareqaziz.aust.cse@gmail.com>
2017-03-29 23:37:40 +0600

    added docker radis image

--- handler.js
+++ handler.js
@@ -1,24 +1,25 @@
 'use strict';
-var AWS = require('aws-sdk');
-var dynamo = new AWS.DynamoDB({endpoint: 'http://localhost:4567'})
+var redis = require("redis");
+var client = redis.createClient(6379);
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
 
 module.exports.hello = (event, context, callback) => {  
+  client.get('wife', function(err, res) {
+    console.log(res); // => 'bar'
 
-  dynamo.listTables(function(err, data) {
-    // console.log(data.TableNames);
     const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      table: data
-      // input: event,
-    }),
-  };
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Your wife's name is !" + res        
+      }),
+    };
 
-  callback(null, response);
+    callback(null, response);
   });
-  
-
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
