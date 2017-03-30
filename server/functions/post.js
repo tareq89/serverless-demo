956b3c458c0bdfce7b2f5d58d1c51b1d337a1648
tareq89 <tareqaziz.aust.cse@gmail.com>
2017-03-30 23:50:03 +0600

    added sns message code

--- server/functions/post.js
+++ server/functions/post.js
@@ -32,6 +32,28 @@ module.exports.handler = (event, context, callback) => {
                 client.del("messagelist");
                 if (amazonKeywordExist) {
                     // TODO: send push notification
+                    // var sns = new AWS.SNS({apiVersion: '2010-03-31'});
+                    // var params = {
+                    //   Message: 'STRING_VALUE', /* required */
+                    //   MessageAttributes: {
+                    //     someKey: {
+                    //       DataType: 'STRING_VALUE', /* required */
+                    //       BinaryValue: new Buffer('...') || 'STRING_VALUE',
+                    //       StringValue: 'STRING_VALUE'
+                    //     },
+                    //     /* anotherKey: ... */
+                    //   },
+                    //   MessageStructure: 'STRING_VALUE',
+                    //   PhoneNumber: 'STRING_VALUE',
+                    //   Subject: 'STRING_VALUE',
+                    //   TargetArn: 'STRING_VALUE',
+                    //   TopicArn: 'STRING_VALUE'
+                    // };
+                    // sns.publish(params, function(err, data) {
+                    //   if (err) console.log(err, err.stack); // an error occurred
+                    //   else     console.log(data);           // successful response
+                    // });
+
                 }
                 client.rpush("messagelist", message);
                 const response = {
